ab21794eb593b52e5e9566abc986acd488fffb59
Davi Emanuel Nogueira <daviemanuel94@gmail.com>
2023-10-18 23:50:04 -0300

    build

---
+++ src/components/Box.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="box has-text-wight-bold" :style="estilos">
+        <slot></slot>
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'box-component',
+    data() {
+        return {
+            estilos: {
+                backgroundColor: '#FAF0CA'
+            }
+        }
+    }
+})
+</script>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,39 @@
+<template>
+    <Box>
+        <div class="columns">
+            <div class="column is-7">
+                {{ tarefa?.descricao || 'Tarefa sem descição' }}
+            </div>
+            <div class="column">
+                <Cronometro :tempoEmSegundos="tarefa?.duracaoEmSegundos"/>
+            </div>
+        </div>
+    </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import Cronometro from './Cronometro.vue';
+import ITarefa from '../interfaces/ITarefa';
+import Box from './Box.vue';
+
+export default defineComponent({
+    name: "tarefa-component",
+    components: {
+        Cronometro,
+        Box
+    },
+    props: {
+        tarefa: {
+            type: Object as PropType<ITarefa>,
+            riquired: true
+        }
+    }
+})
+</script>
+
+<style scoped>
+.box {
+    background: #FAF0CA;
+}
+</style>
