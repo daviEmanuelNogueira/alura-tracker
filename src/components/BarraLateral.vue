3055b53e09748449b0c54bb34f3ebe70a0a2ab48
Davi Emanuel Nogueira <daviemanuel94@gmail.com>
2023-10-21 22:48:49 -0300

    implementando projeto

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -4,50 +4,81 @@
             <img src="../assets/logo.png" alt="logo">
         </h1>
         <button class="button" @click="alterarTema()">{{ textoBotao }}</button>
+        <nav class="panel mt-5">
+            <ul>
+                <li>
+                    <RouterLink to="/" class="link">
+                        <i class="fas fa-tasks"></i>
+                        Tarefas
+                    </RouterLink>
+                </li>
+                <li>
+                    <RouterLink to="/projetos" class="link">
+                        <i class="fas fa-project-diagram"></i>
+                        Projetos
+                    </RouterLink>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
-    export default defineComponent({
-        name: 'BarraLareral-component',
-        emits: ['aoTemaAlterado'],
-        data () {
-            return {
-                modoEscuroAtivo: false
-            }
-        },
-        computed: {
-            textoBotao() {
-                if(this.modoEscuroAtivo) {
-                    return 'Desativar modo escuro'
-                }
-
-                return 'Ativar modo escuro'
-            }
-        },
-        methods: {
-            alterarTema() {
-                this.modoEscuroAtivo = !this.modoEscuroAtivo
-                this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
-            }
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'BarraLareral-component',
+    emits: ['aoTemaAlterado'],
+    data() {
+        return {
+            modoEscuroAtivo: false
         }
-    })
+    },
+    computed: {
+        textoBotao() {
+            if (this.modoEscuroAtivo) {
+                return 'Desativar modo escuro'
+            }
+
+            return 'Ativar modo escuro'
+        }
+    },
+    methods: {
+        alterarTema() {
+            this.modoEscuroAtivo = !this.modoEscuroAtivo
+            this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+        }
+    }
+})
 </script>
 
 <style scoped>
-    header {
-        padding: 1rem;
-        background: #0d3b66;
-        width: 100%;
-        height: 100vh;
-        text-align: center;
-    }
+header {
+    padding: 1rem;
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+}
 
-    @media only screen and (max-width: 768) {
-        header {
-            padding: 2.5rem;
-            height: auto;
-        }
+.panel li {
+    margin: 8px 0;
+}
+
+.link {
+    color: #fff;
+}
+
+.link:hover {
+    color: #FAF0CA;
+}
+
+.link.router-link-active {
+    color: #FAF0CA;
+}
+
+@media only screen and (max-width: 768) {
+    header {
+        padding: 2.5rem;
+        height: auto;
     }
-</style>
+}</style>
